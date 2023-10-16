920ef78fe6aea46332d6320bf01087dbaa580c58
inuside21 <martinmexico3@gmail.com>
2023-10-16 17:06:50 +0800

    Initial commit

--- eetech-terumo-resin.ino
+++ eetech-terumo-resin.ino
@@ -1,41 +1,43 @@
 // WIFI
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
 
 // HTTP
 #include <ESP8266HTTPClient.h>
 
-// server
-String serverAdd = "http://192.168.137.1/";
-
 // device
-int dId1 = 2;                              // device ID
-int dId2 = 3;
-int dId3 = 4;
-String dStatus1 = "";
-String dStatus2 = "";
-String dStatus3 = "";
+int dId1 = 4;                              // device ID
+int dId2 = 5;
+int dId3 = 6;
+int dFlag1 = 0;                            // device status : 0 - normal | 1 - alarm
+int dFlag2 = 0; 
+int dFlag3 = 0; 
 
 // input
 int pin1 = D1;                            // emergency light 1
 int pin2 = D2;                            // emergency light 2
 int pin3 = D5;                            // emergency light 3
+int reading1;
+int reading2;
+int reading3;
 
 // output
 int outpin1 = D0;                         // tower light
 
 // wifi
-WiFiClient nodeclient;
-const char* ssid     = "teteteet";
+const char* ssid     = "EETechAnalog";
 const char* password = "12345678";
 
-// wifi manual
-//IPAddress local_IP(192, 168, 50, 146);          // Terumo
+//const char* ssid     = "FTTX-fd06d4";   // Terumo
+//const char* password = "d4239174";
+//IPAddress local_IP(192, 168, 50, 146);          
 //IPAddress gateway(192, 168, 50, 144);
 //IPAddress subnet(255, 255, 255, 240);
 //IPAddress primaryDNS(8, 8, 8, 8);   //optional
 //IPAddress secondaryDNS(8, 8, 4, 4); //optional
 
 // http
+WiFiClient wclient;
 HTTPClient http1;
 HTTPClient http2;
 HTTPClient http3;
@@ -48,7 +50,7 @@ int httpCode3;
 // ================
 // START
 // ================
-void setup()
+void setup() 
 {
   // init serial
   Serial.begin(9600);
@@ -59,13 +61,13 @@ void setup()
   pinMode(pin3, INPUT_PULLUP);
 
   /*
-    // init wifi manual
-    if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+  // init wifi station
+  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
     Serial.println("Resin Equipment Monitoring - Wifi Setup Failed");
-    }
-    else {
+  }
+  else {
     Serial.println("Resin Equipment Monitoring - Wifi Setup Success");
-    }
+  }
   */
 
   // init wifi connection
@@ -86,15 +88,14 @@ void setup()
 // ================
 // LOOP
 // ================
-void loop()
+void loop() 
 {
-  // input read
-  int reading1 = digitalRead(pin1);
-  int reading2 = digitalRead(pin2);
-  int reading3 = digitalRead(pin3);
+  reading1 = digitalRead(pin1);
+  reading2 = digitalRead(pin2);
+  reading3 = digitalRead(pin3);
 
   // check wifi
-  if (WiFi.status() != WL_CONNECTED)
+  if (WiFi.status() != WL_CONNECTED) 
   {
     // exit
     delay(500);
@@ -102,75 +103,70 @@ void loop()
   }
 
   // check input 1
-  // ---------------------------
-  if (!reading1)
+  if (!reading1) 
   {
-    dStatus1 = "active";
+    dFlag1 = 0;
     digitalWrite(outpin1, LOW);
   }
-  else
+  else 
   {
-    dStatus1 = "online";
+    dFlag1 = 1;
     digitalWrite(outpin1, HIGH);
   }
 
   // check input 2
-  // ---------------------------
-  if (!reading2)
+  if (!reading2) 
   {
-    dStatus2 = "active";
+    dFlag2 = 0;
     digitalWrite(outpin1, LOW);
   }
-  else
+  else 
   {
-    dStatus2 = "online";
+    dFlag2 = 1;
     digitalWrite(outpin1, HIGH);
   }
 
   // check input 3
-  // ---------------------------
-  if (!reading3)
+  if (!reading3) 
   {
-    dStatus3 = "active";
+    dFlag3 = 0;
     digitalWrite(outpin1, LOW);
   }
-  else
+  else 
   {
-    dStatus3 = "online";
+    dFlag3 = 1;
     digitalWrite(outpin1, HIGH);
   }
 
   // send data - 1
-  http1.begin(nodeclient, serverAdd + "eetech-monitoring/server/api.php?mode=1&id=" + String(dId1) + "&st=" + dStatus1);    // device id, device status
+  http1.begin(wclient, "http://192.168.137.56/project-equipment/receiver.php?d=" + String(dId1) + "•" + String(dFlag1));    // device id, device status
   httpCode1 = http1.GET();
   http1.end();
 
   // send data - 2
-  http2.begin(nodeclient, serverAdd + "eetech-monitoring/server/api.php?mode=1&id=" + String(dId2) + "&st=" + dStatus2);    // device id, device status
+  http2.begin(wclient, "http://192.168.137.56/project-equipment/receiver.php?d=" + String(dId2) + "•" + String(dFlag2));    // device id, device status
   httpCode2 = http2.GET();
   http2.end();
 
   // send data - 3
-  http3.begin(nodeclient, serverAdd + "eetech-monitoring/server/api.php?mode=1&id=" + String(dId3) + "&st=" + dStatus3);    // device id, device status
+  http3.begin(wclient, "http://192.168.137.56/project-equipment/receiver.php?d=" + String(dId3) + "•" + String(dFlag3));    // device id, device status
   httpCode3 = http3.GET();
   http3.end();
-
-  /*
-    // check reply
-    if (httpCode2 > 0) {
-      if (httpCode2 == HTTP_CODE_OK) {
-        Serial.println("Resin Equipment Monitoring - HTTP Success");
-      }
-      else {
-        Serial.println("Resin Equipment Monitoring - HTTP Failed[1] - " + String(httpCode2));
-        digitalWrite(LED_BUILTIN, HIGH);
-      }
+  
+  // check reply
+  if(httpCode2 > 0) {
+    if(httpCode2 == HTTP_CODE_OK) {
+      Serial.println("Resin Equipment Monitoring - HTTP Success");
     }
     else {
-      Serial.println("Resin Equipment Monitoring - HTTP Failed[0]");
+      Serial.println("Resin Equipment Monitoring - HTTP Failed[1] - " + String(httpCode2));
+       digitalWrite(LED_BUILTIN, HIGH);  
     }
-  */
-
+  }
+  else {
+      Serial.println("Resin Equipment Monitoring - HTTP Failed[0]");
+  }
+  
 
   // exit
   delay(1000);
